ea7216c9321e5a3645cada6f6573ca88d15d22ea
jabarteley <zagajabar1@gmail.com>
2024-07-11 12:28:18 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//swiper
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+/*
+ <link rel="stylesheet"
+   href="https://unpkg.com/swiper/swiper-bundle.min.css"
+    />
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+        <!-- Swiper JS -->
+var swiper = new swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+   
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
+    */
